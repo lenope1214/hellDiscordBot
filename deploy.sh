9b12034f4100282ce1928c5ad7bc514134ad0056
이성복 <dltjdqhr1000@gmail.com>
2023-02-16 05:19:19 +0900

    [UPDATE] deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 rm hellbot.jar || true
 ./gradlew clean build --stacktrace
 
-fuser -k $(jps -lv | awk '/hellbot.jar/ {print $1}')/tcp
+#fuser -k $(jps -lv | awk '/hellbot.jar/ {print $1}')/tcp
+kill $(jps -lv | awk '/hellbot.jar/ {print $1}')
 
 cp build/libs/*.jar .
 nohup java -jar hellbot.jar > /dev/null 2>&1 &
