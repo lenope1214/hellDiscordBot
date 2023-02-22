60dd13ab84a25920106a2dd019a24f8cd2741350
leeseongbok <lsb@j-sol.co.kr>
2023-02-22 18:33:01 +0900

    [UPDATE] docker-compose 빌드 수정

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,14 @@
 #  기존 hellbot image 삭제
 docker rmi lenope1214/hellbot
 
-docker build -t lenope1214/hellbot .
+docker build -t lenope1214/hellbot:latest .
 
 ## 기존에 돌아가던 컨테이너 삭제
 #docker ps -q --filter "name=laundry-prod-server" | grep -q . && docker stop laundry-prod-server && docker rm laundry-prod-server | true
 docker-compose down
+
+rm -rf build || true
+rm -f hellbot.jar
 
 ## 빌드한 이미지 실행
 #docker run -p 10100:10100 -p 10101:10101 -d --name=hellbot lenope1214/hellbot -v logs:/logs
